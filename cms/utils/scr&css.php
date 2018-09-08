80a1e2157de6ada6ed18395e7a27788868a22334
TretyakVictor <victortretyak@protonmail.com>
2018-09-08 13:55:18 +0300

    #1 css and js separated

--- cms/utils/scr&css.php
+++ cms/utils/scr&css.php
@@ -1,6 +1,6 @@
-<link href="../../css/bootstrap.css" rel="stylesheet" media="screen" charset="utf-8">
-<link rel="stylesheet" href="../../css/main.css" media="screen" title="no title" charset="utf-8">
-<script src="../../js/jquery-2.2.0.min.js"></script>
-<script src="../../js/bootstrap.min.js"></script>
-<script src="../../js/rainbow-custom.min.js"></script>
-<link href="../../css/rainbow-monokai.css" rel="stylesheet" media="screen">
+<link href="../../css/libs/bootstrap.css" rel="stylesheet" media="screen" charset="utf-8">
+<link rel="stylesheet" href="../../css/admin_styles.css" media="screen" title="no title" charset="utf-8">
+<script src="../../js/libs/jquery-2.2.0.min.js"></script>
+<script src="../../js/libs/bootstrap.min.js"></script>
+<script src="../../js/libs/rainbow-custom.min.js"></script>
+<link href="../../css/libs/rainbow-monokai.css" rel="stylesheet" media="screen">
